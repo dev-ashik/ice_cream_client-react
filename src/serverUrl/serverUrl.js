193d38ca801bbe62622d1147103e98b9706e65a5
MD ASHIK MAHMUD <61373940+dev-ashik@users.noreply.github.com>
2023-06-08 09:07:51 +0800

    change

--- src/serverUrl/serverUrl.js
+++ src/serverUrl/serverUrl.js
@@ -1,6 +1,6 @@
-const serverUrl = 'http://localhost:8000'
+// const serverUrl = 'http://localhost:8000'
 
-// const serverUrl = 'https://sugar-cone-corner-server.onrender.com'
+const serverUrl = 'https://sugar-cone-corner-server.onrender.com'
 
 
 export { serverUrl }
